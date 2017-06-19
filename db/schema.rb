b94915e88966753101cae460d7eefea742869b47
Roman Serikov <roman.serikov@yandex.ru>
2017-06-19 22:15:47 +0300

    Add train resource via scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619191003) do
+ActiveRecord::Schema.define(version: 20170619191350) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
