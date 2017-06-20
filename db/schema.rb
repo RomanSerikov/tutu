03a1574f5ac2920e3f58b1487be3bbf8b8374f3d
Roman Serikov <roman.serikov@yandex.ru>
2017-06-20 23:50:08 +0300

    Add Routes migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619191350) do
+ActiveRecord::Schema.define(version: 20170620204824) do
 
   create_table "railway_stations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
