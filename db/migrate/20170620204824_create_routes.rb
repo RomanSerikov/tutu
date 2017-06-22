8bec48566e6aeb756554cb7e3f67ac048f59f0fa
Roman Serikov <roman.serikov@yandex.ru>
2017-06-22 21:19:40 +0300

    Fix Lesson 12

--- db/migrate/20170620204824_create_routes.rb
+++ db/migrate/20170620204824_create_routes.rb
@@ -1,7 +1,7 @@
 class CreateRoutes < ActiveRecord::Migration[5.1]
   def change
     create_table :routes do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170620204824) do
   end
 
   create_table "routes", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
