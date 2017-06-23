9bc177ace497dcd27e09718bb126dfbd54a1103d
Roman Serikov <roman.serikov@yandex.ru>
2017-06-23 19:34:23 +0300

    Refactor Lesson 13

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,8 +1,7 @@
 class Route < ApplicationRecord
-  validates :title, presence: true
-
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
-  
   has_many :trains
+
+  validates :title, presence: true
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,5 +1,6 @@
 class Train < ApplicationRecord
   belongs_to :route
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: current_station_id
+  
   has_many :tickets
 end
