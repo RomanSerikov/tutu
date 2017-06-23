7a6c049cd0f79b457eb509e76394eb4fb7de6dc9
Roman Serikov <roman.serikov@yandex.ru>
2017-06-23 19:18:09 +0300

    Add Ticket and User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622203501) do
+ActiveRecord::Schema.define(version: 20170623161543) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -29,12 +29,22 @@ ActiveRecord::Schema.define(version: 20170622203501) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "trains", force: :cascade do |t|
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "route_id"
     t.index ["route_id"], name: "index_trains_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
