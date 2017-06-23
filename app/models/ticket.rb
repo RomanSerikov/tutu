c2be89acd8ff9ae54266f7536b50be681df7b1c0
Roman Serikov <roman.serikov@yandex.ru>
2017-06-23 19:28:51 +0300

    Add User-Ticket association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :train
   belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623162224) do
+ActiveRecord::Schema.define(version: 20170623162709) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -35,9 +35,11 @@ ActiveRecord::Schema.define(version: 20170623162224) do
     t.integer "train_id"
     t.integer "start_station_id"
     t.integer "end_station_id"
+    t.integer "user_id"
     t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
     t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|
