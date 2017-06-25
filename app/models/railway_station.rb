cea29195c571de7c25555fcdcdd543520fffbe9a
Roman Serikov <roman.serikov@yandex.ru>
2017-06-25 22:28:44 +0300

    Fix Train - RailwayStation association

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,5 +2,5 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :tickets
-  has_many :trains
+  has_many :trains, foreign_key: :current_station_id
 end
