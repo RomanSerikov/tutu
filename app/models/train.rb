05547ec3ac9194359bcd235a4f3288ab922f9efe
Roman Serikov <roman.serikov@yandex.ru>
2017-06-27 22:25:10 +0300

    Remove foreign key from Train model

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,6 @@
 class Train < ApplicationRecord
   belongs_to :route
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :current_station, class_name: 'RailwayStation'
   
   has_many :tickets
 end
