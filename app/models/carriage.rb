2ddb1856099c7aea4c00e9b7b1d6f83d07d7ec54
Roman Serikov <roman.serikov@yandex.ru>
2017-07-03 21:25:21 +0300

    Add inclusion validation to Carriage

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,5 +1,8 @@
 class Carriage < ApplicationRecord
-  CARRIAGE_TYPES = ['Купе', 'Плацкарт']
+  CARRIAGE_TYPES = %w[coupe platscard]
 
   belongs_to :train
+
+  validates :carriage_type, inclusion: { in: CARRIAGE_TYPES,
+    message: "%{value} is not a valid carriage type" }
 end
