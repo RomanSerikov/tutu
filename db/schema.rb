6dc2de35b4619a60232bf1734048f1e15e77bb8f
Roman Serikov <roman.serikov@yandex.ru>
2017-07-04 00:57:50 +0300

    Add station position in Route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170629220201) do
+ActiveRecord::Schema.define(version: 20170703211525) do
 
   create_table "carriages", force: :cascade do |t|
     t.string "carriage_type", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170629220201) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "station_position"
   end
 
   create_table "routes", force: :cascade do |t|
