27865947aa22274a13cf4af5015533fb1e7618ba
Roman Serikov <roman.serikov@yandex.ru>
2017-07-11 21:11:03 +0300

    Add stations sort in Route via form

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,7 +4,24 @@ class RailwayStation < ApplicationRecord
   has_many :tickets
   has_many :trains, foreign_key: :current_station_id
 
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.station_position").uniq }
+
+  # def update_position(route, position)
+  #   railway_stations_routes.find_by(route: route).update(station_position: position)
+  # end
+
   def update_position(route, position)
-    railway_stations_routes.find_by(route: route).update(station_position: position)
+    station_route = station_route(route)
+    station_route.update(station_position: position) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:station_position)
+  end
+
+  protected
+
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
   end
 end
--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -2,5 +2,5 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
 
-  default_scope { order(:station_position) }
+  # default_scope { order(:station_position) }
 end
