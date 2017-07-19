78ee4a289dcaa6f389543ad8d258c2716094d993
Roman Serikov <roman.serikov@yandex.ru>
2017-07-19 19:37:00 +0300

    Fix: admin flag migration

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema.define(version: 20170718183534) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
