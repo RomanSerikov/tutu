20131876d7c50f4c17721e4825d3824ddd640b79
Roman Serikov <roman.serikov@yandex.ru>
2017-07-26 22:35:53 +0300

    Add: ticket mailer and mail sending after ticket purchase

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,16 @@ class Ticket < ApplicationRecord
   belongs_to :start_station, class_name: 'RailwayStation'
   belongs_to :end_station, class_name: 'RailwayStation'
   belongs_to :user
+
+  after_create :send_notification
+
+  def route_name
+    "#{start_station.title} - #{end_station.title}"
+  end
+
+  private
+
+    def send_notification
+      TicketsMailer.buy_ticket(self.user, self).deliver_now
+    end
 end
