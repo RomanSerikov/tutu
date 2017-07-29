ff9a072ac6b436f58c40553683d9266defcc391d
Roman Serikov <roman.serikov@yandex.ru>
2017-07-29 14:17:50 +0300

    Add: inline edit for railway stations

---
+++ app/assets/javascripts/railway_stations.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $('a.edit_station').click(function(e) {
+    e.preventDefault();
+    var station_id;
+    var form;
+    var title;
+
+    station_id = $(this).data('stationId');
+    form = $('#edit_railway_station_' + station_id);
+    title = $('#railway_station_title_' + station_id);
+
+    if (!$(this).hasClass('cancel')) {
+      $(this).html('Отменить');
+      $(this).addClass('cancel');
+    } else {
+      $(this).html('Изменить');
+      $(this).removeClass('cancel');
+    }
+
+    form.toggle();
+    title.toggle();
+
+  });
+});
