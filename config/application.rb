099b289705ee6d8e2cebb379d4671a314fcd9d72
Roman Serikov <roman.serikov@yandex.ru>
2017-08-07 18:30:24 +0300

    Add: config for shoulda-matchers and rails generators

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module Tutu
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ru
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false,
+                       controller_specs: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
