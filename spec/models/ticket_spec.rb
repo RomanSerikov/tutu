9d47d5f99ac95c43ee62eec62faba78152327728
Roman Serikov <roman.serikov@yandex.ru>
2017-12-14 22:50:55 +0300

    Add: ticket model spec

---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Ticket, type: :model do
+  it { should belong_to(:train) }
+  it { should have_db_column(:train_id) }
+  it { should belong_to(:start_station).class_name('RailwayStation') }
+  it { should have_db_column(:start_station_id) }
+  it { should belong_to(:end_station).class_name('RailwayStation') }
+  it { should have_db_column(:end_station_id) }
+  it { should belong_to(:user) }
+  it { should have_db_column(:user_id) }
+
+  describe '#route_name' do
+    let(:start)  { create(:railway_station, title: 'Moscow') }
+    let(:finish) { create(:railway_station, title: 'Saint-Peterburg') }
+    let(:ticket) { build(:ticket, start_station: start, end_station: finish) }
+
+    it 'returns string contains start start and end stations' do
+      expect(ticket.route_name).to eq 'Moscow - Saint-Peterburg'
+    end
+  end
+end
